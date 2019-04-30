a727c313faf6a65f37ecb666d8d6fe9d5a244b73
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2019-04-30 19:19:22 +0200

    init

---
+++ app/src/main/java/io/github/jozefchmelar/MainActivity.kt
@@ -0,0 +1,14 @@
+package io.github.jozefchmelar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        app_version.text = "${BuildConfig.VERSION_CODE}"
+    }
+}
