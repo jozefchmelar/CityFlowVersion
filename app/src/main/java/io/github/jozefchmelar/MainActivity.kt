adfabaed66937ad03d772fa4850f4588b84e09f9
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2019-04-30 20:25:16 +0200

    changed the app_version text

--- app/src/main/java/io/github/jozefchmelar/MainActivity.kt
+++ app/src/main/java/io/github/jozefchmelar/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        app_version.text = "${BuildConfig.VERSION_CODE}"
+        app_version.text = "${BuildConfig.VERSION_NAME}  ${BuildConfig.VERSION_CODE} "
     }
 }
